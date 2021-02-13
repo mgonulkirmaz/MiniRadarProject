ee9042d12da0871a79528ccb26f1b34fbfd13193
Mustafa <mgonulkrmaz@gmail.com>
2021-02-14 02:41:09 +0300

    Some GUI Visual adjustments

---
+++ MiniRadarUI/MainForm.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace MiniRadarUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.leftPanel = new System.Windows.Forms.Panel();
+            this.settingsButton = new System.Windows.Forms.Button();
+            this.exitAppButton = new System.Windows.Forms.Button();
+            this.topPanel = new System.Windows.Forms.Panel();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.leftPanel.SuspendLayout();
+            this.topPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // leftPanel
+            // 
+            this.leftPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.leftPanel.Controls.Add(this.settingsButton);
+            this.leftPanel.Controls.Add(this.exitAppButton);
+            this.leftPanel.Location = new System.Drawing.Point(0, 24);
+            this.leftPanel.Name = "leftPanel";
+            this.leftPanel.Size = new System.Drawing.Size(200, 936);
+            this.leftPanel.TabIndex = 0;
+            // 
+            // settingsButton
+            // 
+            this.settingsButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.settingsButton.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.settingsButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.settingsButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.settingsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.settingsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.settingsButton.Location = new System.Drawing.Point(20, 760);
+            this.settingsButton.Name = "settingsButton";
+            this.settingsButton.Size = new System.Drawing.Size(180, 60);
+            this.settingsButton.TabIndex = 1;
+            this.settingsButton.Text = "SETTINGS";
+            this.settingsButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.settingsButton.UseVisualStyleBackColor = false;
+            // 
+            // exitAppButton
+            // 
+            this.exitAppButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.exitAppButton.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.exitAppButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.exitAppButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.exitAppButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitAppButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.exitAppButton.Location = new System.Drawing.Point(20, 840);
+            this.exitAppButton.Name = "exitAppButton";
+            this.exitAppButton.Size = new System.Drawing.Size(180, 60);
+            this.exitAppButton.TabIndex = 0;
+            this.exitAppButton.Text = "EXIT";
+            this.exitAppButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.exitAppButton.UseVisualStyleBackColor = false;
+            this.exitAppButton.Click += new System.EventHandler(this.exitAppButton_Click);
+            // 
+            // topPanel
+            // 
+            this.topPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.topPanel.Controls.Add(this.timeLabel);
+            this.topPanel.Controls.Add(this.dateLabel);
+            this.topPanel.Location = new System.Drawing.Point(0, 0);
+            this.topPanel.Name = "topPanel";
+            this.topPanel.Size = new System.Drawing.Size(1440, 26);
+            this.topPanel.TabIndex = 1;
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(1280, 3);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(49, 20);
+            this.timeLabel.TabIndex = 1;
+            this.timeLabel.Text = "00:00";
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.Location = new System.Drawing.Point(1340, 3);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(89, 20);
+            this.dateLabel.TabIndex = 0;
+            this.dateLabel.Text = "14.02.2021";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.ClientSize = new System.Drawing.Size(1438, 958);
+            this.ControlBox = false;
+            this.Controls.Add(this.topPanel);
+            this.Controls.Add(this.leftPanel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.leftPanel.ResumeLayout(false);
+            this.topPanel.ResumeLayout(false);
+            this.topPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel leftPanel;
+        private System.Windows.Forms.Panel topPanel;
+        private System.Windows.Forms.Label dateLabel;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Button exitAppButton;
+        private System.Windows.Forms.Button settingsButton;
+    }
+}
+
---
+++ MiniRadarUI/MainForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniRadarUI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exitAppButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        Timer timerOneSec;
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            timerOneSec = new Timer();
+            timerOneSec.Interval = 1000;
+            timerOneSec.Tick += TimerOneSecTick;
+            timerOneSec.Start();
+        }
+
+        private void TimerOneSecTick(object sender, EventArgs e)
+        {
+            dateLabel.Text = DateTime.Now.Date.ToShortDateString();
+            timeLabel.Text = DateTime.Now.ToShortTimeString();
+        }
+    }
+}
