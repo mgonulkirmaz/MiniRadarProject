193a9578ecea5e1c27b1ba36043cbc219fbde2c4
Mustafa <mgonulkrmaz@gmail.com>
2021-02-15 00:31:56 +0300

    Added some functions.

--- MiniRadarUI/Parser.cs
+++ MiniRadarUI/Parser.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace MiniRadarUI
 {
-    class Parser
+    public class Parser
     {
+
+        public static Utilities.Node Split(string input)   //Splits input string to Angle and Distance values
+        {
+            string[] inputSplitted = input.Split(';');
+            Utilities.Node node;
+
+            if (Utilities.CommandControl(inputSplitted[0]) == Utilities.COMMAND.NEW_DATA)
+            {
+                node.Command = Utilities.COMMAND.NEW_DATA;
+                node.Angle = Convert.ToDouble(inputSplitted[1]);
+                node.Distance = Convert.ToDouble(inputSplitted[2]);
+            }
+            else
+            {
+                node.Command = Utilities.COMMAND.UNKNOWN_COMMAND;
+                node.Angle = 0;
+                node.Distance = 0;
+            }
+                
+            return node;
+        }
+
+        public static Utilities.CommData CreateCommData(Utilities.COMMAND cmd, string data)
+        {
+            Utilities.CommData CommData;
+            CommData.Command = cmd;
+            CommData.Data = data;
+
+            return CommData;
+        }
     }
 }
